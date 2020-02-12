24073c90cdf69e62ec21de0e7a6d99c0c27e4c82
skrebetsnataliia <skrebetsnatasha98@gmail.com>
2020-02-12 15:40:00 +0200

    react-shop

---
+++ precache-manifest.34a2b394e2a7c121cd4c19b7a9a68fdc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621cdb87556c6a3ae9f27fcfa67ac6eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24aade0d8e8ba32ee1db",
+    "url": "/static/css/main.929ca715.chunk.css"
+  },
+  {
+    "revision": "ea33d52d0f5c367e016e",
+    "url": "/static/js/2.469cd8fe.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.469cd8fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24aade0d8e8ba32ee1db",
+    "url": "/static/js/main.8e164a7f.chunk.js"
+  },
+  {
+    "revision": "c2ad5c23fb2789f3f3dd",
+    "url": "/static/js/runtime-main.903fb561.js"
+  },
+  {
+    "revision": "ade27dc73e85f621cc42fdfd5bfaf1f9",
+    "url": "/static/media/cart.ade27dc7.png"
+  },
+  {
+    "revision": "939c3220a272df7d26e9734ea2cbba17",
+    "url": "/static/media/item1.939c3220.jpeg"
+  },
+  {
+    "revision": "563074f62e600ef7fa01034b868feefb",
+    "url": "/static/media/item2.563074f6.jpeg"
+  },
+  {
+    "revision": "e86a12131b026ed351b8750a30df8082",
+    "url": "/static/media/item3.e86a1213.jpg"
+  },
+  {
+    "revision": "21c3e4c5d547e1be4dc48ff85358879a",
+    "url": "/static/media/item4.21c3e4c5.jpg"
+  },
+  {
+    "revision": "67e539a5ec0d2a27f3aadd86c197c2aa",
+    "url": "/static/media/item5.67e539a5.jpeg"
+  },
+  {
+    "revision": "14d7bc8dc043dc8cefd874b2bfcf2b9c",
+    "url": "/static/media/item6.14d7bc8d.jpeg"
+  },
+  {
+    "revision": "6b345fa1bdb9f8dbe4619df7a2f6417f",
+    "url": "/static/media/item7.6b345fa1.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
